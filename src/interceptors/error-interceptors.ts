ea1effdf260c39b8c9af0484086f56b53712562e
Fernando Henrique <fernando.hnrq@gmail.com>
2019-04-06 16:32:36 -0300

    Tratamento padrao para outros erros com Alert

--- src/interceptors/error-interceptors.ts
+++ src/interceptors/error-interceptors.ts
@@ -1,13 +1,15 @@
-import { Injectable } from '@angular/core';
+import { StatusBar } from '@ionic-native/status-bar/ngx';
+import { Injectable, enableProdMode } from '@angular/core';
 import { HttpEvent, HttpInterceptor, HttpHandler, HttpRequest, HTTP_INTERCEPTORS } from '@angular/common/http';
 import { Observable } from 'rxjs'; // IMPORTANTE: IMPORT ATUALIZADO
 import { catchError } from 'rxjs/operators';
 import { StorageService } from 'src/services/storage.service';
+import { AlertController } from '@ionic/angular';
 
 @Injectable()
 export class ErrorInterceptor implements HttpInterceptor {
 
-    constructor(public storage: StorageService) {
+    constructor(public storage: StorageService, public alertController: AlertController) {
     }
 
     intercept(req: HttpRequest<any>, next: HttpHandler): Observable<HttpEvent<any>> {
@@ -27,16 +29,48 @@ export class ErrorInterceptor implements HttpInterceptor {
                 console.log(errorObj);
 
                 switch(errorObj.status) {
+                    case 401:
+                    this.handle401();
+                    break;
                     case 403:
                     this.handle403();
+                    break;
+                    default:
+                    this.handleDefaultError(errorObj);
                     break;
                 }
 
                 return Observable.throw(errorObj);
             }))as any;
     }
+    async handleDefaultError(errorObj){
+        const alert = await this.alertController.create({
+            header: 'Erro' + errorObj.status + ": "+ errorObj.error,
+            message: errorObj.message,
+            buttons: [
+                {
+                    text:'OK'
+                 }
+        ]
+        });
+        await alert.present();
+    }
     handle403() {
         this.storage.setLocalUser(null);
+    }
+
+    async handle401(){
+        const alert = await this.alertController.create({
+            header: 'Erro 401:',
+            subHeader:'falha de autenticação',
+            message: 'Email ou senha incorretos',
+            buttons: [
+                {
+                    text:'OK'
+                 }
+        ]
+        });
+        await alert.present();
     }
 }
    
